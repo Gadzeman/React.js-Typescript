c0d2e4ed98d6412f2a9b667d96ba419197d03d43
Gadzeman <ostap778800@gmail.com>
2021-08-04 12:41:59 +0300

    Events example

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import List from "./components/List/List";
 import UserItem from "./components/UserList/UserItem/UserItem";
 import TodoItem from "./components/TodoItem/TodoItem";
+import EventsExample from "./components/EventsExample/EventsExample";
 
 const App = () => {
     const [users, setUsers] = useState<IUser[]>([])
@@ -30,18 +31,23 @@ const App = () => {
     }, [])
     return (
         <div>
-            <List
-                items={users}
-                renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
-            />
-            <List
-                items={todos}
-                renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
-            />
+            <EventsExample />
+
+            {/*<List*/}
+            {/*    items={users}*/}
+            {/*    renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}*/}
+            {/*/>*/}
+            {/*<List*/}
+            {/*    items={todos}*/}
+            {/*    renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}*/}
+            {/*/>*/}
+
             {/*<UserList users={users}/>*/}
+
             {/*<Card onClick={(num) => console.log("Pressed", num)} variant={CardVariant.primary} width={"200px"} height={"200px"}>*/}
             {/*    <button>Кнопка</button>*/}
             {/*</Card>*/}
+
         </div>
     );
 };
