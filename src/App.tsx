8e396acfd16bb05c0fdd9ec25f86d89ff398a134
Gadzeman <ostap778800@gmail.com>
2021-08-06 22:42:38 +0300

    Branch working with typescript and redux is made

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import React from 'react';
-import {BrowserRouter, Route, NavLink} from "react-router-dom";
-import UsersPage from "./components/UsersPage/UsersPage";
-import TodosPage from "./components/TodosPage/TodosPage";
-import UserItemPage from "./components/UsersItemPage/UserItemPage";
-import TodoItemPage from "./components/TodoItemPage/TodoItemPage";
+import React, {FC} from 'react';
 
-const App = () => {
+const App: FC = () => {
     return (
-        <BrowserRouter>
-            <div>
-                <div>
-                    <NavLink to={"/users"}>Users</NavLink>
-                    <NavLink to={"/todos"}>Todos</NavLink>
-                </div>
-                <Route path={"/users"} exact>
-                    <UsersPage />
-                </Route>
-                <Route path={"/todos"} exact>
-                    <TodosPage />
-                </Route>
-                <Route path={"/users/:id"}>
-                    <UserItemPage />
-                </Route>
-                <Route path={"/todos/:id"}>
-                    <TodoItemPage />
-                </Route>
+        <div>
 
-                {/*<EventsExample />*/}
-
-                {/*<UserList users={users}/>*/}
-
-                {/*<Card onClick={(num) => console.log("Pressed", num)} variant={CardVariant.primary} width={"200px"} height={"200px"}>*/}
-                {/*    <button>Кнопка</button>*/}
-                {/*</Card>*/}
-            </div>
-        </BrowserRouter>
+        </div>
     );
 };
 
